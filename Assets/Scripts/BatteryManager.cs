ce0decae96916c3510a289aa26d143c91fc3213b
Andy Henriquez <an682928@ucf.edu>
2025-08-10 21:59:39 -0400

    Tryinng to fix rotations

--- Assets/Scripts/BatteryManager.cs
+++ Assets/Scripts/BatteryManager.cs
@@ -1,23 +1,27 @@
 using Photon.Pun;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BatteryManager : MonoBehaviour
 {
     [SerializeField] private float holdingMovementSpeed = 3f;
     [SerializeField] private float holdingSprintSpeed = 4.5f;
-    [SerializeField] private float regularMovementSpeed;
-    [SerializeField] private float regularSprintSpeed;
+    private float regularMovementSpeed;
+    private float regularSprintSpeed;
 
     [SerializeField] private GameObject m_battery;
     [SerializeField] private GameObject m_charger;
-    [SerializeField] private float m_numberOfBatterys = 5f;
-    [SerializeField] private float m_numberOfBatteryChargers = 5f;
+    [SerializeField] private GameObject m_batterySpawns;
+    [SerializeField] private GameObject m_chargerSpawns;
+    [SerializeField] private int m_numberOfBatterys = 5;
+    [SerializeField] private int m_numberOfBatteryChargers = 5;
     [SerializeField] private float m_useMagnitude = 1f;
 
     [SerializeField] private KeyCode m_useKey = KeyCode.F;
     [SerializeField] private KeyCode m_dropKey = KeyCode.G;
 
     private bool m_holdingBattery = false;
+    private GameObject heldBattery;
 
     private LayerMask layerMask;
     static readonly int batteryLayer = 11;
@@ -33,7 +37,8 @@ public class BatteryManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SpawnBatterys();
+        SpawnBatteryChargers();
     }
 
     // Update is called once per frame
@@ -50,14 +55,57 @@ public class BatteryManager : MonoBehaviour
         }
     }
 
-    private void SpawnBatterys()
+    void SpawnBatterys()
     {
+        if (!PhotonNetwork.IsMasterClient) return;
 
+        Transform parent = m_batterySpawns.transform;
+        int available = parent.childCount;
+        int toSpawn = Mathf.Clamp(m_numberOfBatterys, 0, available);
+
+        if (toSpawn < m_numberOfBatterys)
+            Debug.LogWarning($"BatteryManager: Requested {m_numberOfBatterys} batteries but only {available} spawn points. Spawning {toSpawn}.");
+
+        foreach (int idx in GetUniqueRandomIndices(available, toSpawn))
+        {
+            Transform sp = parent.GetChild(idx);
+            PhotonNetwork.Instantiate(m_battery.name, sp.position, m_battery.transform.rotation);
+        }
     }
 
-    private void SpawnBatteryChargers()
+    void SpawnBatteryChargers()
     {
+        if (!PhotonNetwork.IsMasterClient) return;
 
+        Transform parent = m_chargerSpawns.transform;
+        int available = parent.childCount;
+        int toSpawn = Mathf.Clamp(m_numberOfBatteryChargers, 0, available);
+
+        if (toSpawn < m_numberOfBatteryChargers)
+            Debug.LogWarning($"BatteryManager: Requested {m_numberOfBatteryChargers} chargers but only {available} spawn points. Spawning {toSpawn}.");
+
+        foreach (int idx in GetUniqueRandomIndices(available, toSpawn))
+        {
+            Transform sp = parent.GetChild(idx);
+            Quaternion baseRot = m_charger.transform.rotation; // prefab's default rotation (-90 X baked in)
+            Quaternion rot = Quaternion.Euler(-90f, sp.eulerAngles.y, sp.eulerAngles.z); // spawn point's Y rotation
+            PhotonNetwork.Instantiate(m_charger.name, sp.position, rot);
+        }
+    }
+
+    static IEnumerable<int> GetUniqueRandomIndices(int poolSize, int pickCount)
+    {
+        // build 0..n-1
+        int[] arr = new int[poolSize];
+        for (int i = 0; i < poolSize; i++) arr[i] = i;
+
+        // partial shuffle
+        for (int i = 0; i < pickCount; i++)
+        {
+            int j = Random.Range(i, poolSize);
+            (arr[i], arr[j]) = (arr[j], arr[i]);
+            yield return arr[i];
+        }
     }
 
     private void AttemptBatteryGrabPlace()
@@ -69,24 +117,30 @@ public class BatteryManager : MonoBehaviour
 
         Debug.Log($"hit object {hit.collider.name}");
 
+        // Grab Battery
         if (hit.collider.gameObject.layer == batteryLayer && !m_holdingBattery)
         {
-            // Place battery infront of player
+            m_holdingBattery = true;
+
+            Destroy(hit.collider.gameObject);
+            heldBattery = Instantiate(m_battery, transform.Find("PlayerBatteryPlacePoint").position, transform.Find("PlayerBatteryPlacePoint").rotation);
+            heldBattery.GetComponent<Collider>().enabled = false;
             GetComponent<PlayerController>().movementSpeed = holdingMovementSpeed;
             GetComponent<PlayerController>().sprintSpeed = holdingSprintSpeed;
-            m_holdingBattery = true;
         }
 
+        // Place Battery
         if (hit.collider.gameObject.layer == chargerLayer && m_holdingBattery)
         {
-            // Hide battery infront of player
+            m_holdingBattery = false;
+
+            Destroy(heldBattery);
             GetComponent<PlayerController>().movementSpeed = regularMovementSpeed;
             GetComponent<PlayerController>().sprintSpeed = regularSprintSpeed;
 
             GameObject modifiedBattery = Instantiate(m_battery, hit.collider.gameObject.transform.Find("BatteryPlacePoint").position, hit.collider.gameObject.transform.Find("BatteryPlacePoint").rotation);
             hit.collider.enabled = false;
             modifiedBattery.GetComponent<Collider>().enabled = false;
-            m_holdingBattery = false;
         }
     }
 
@@ -99,7 +153,7 @@ public class BatteryManager : MonoBehaviour
 
         Debug.Log($"hit object {hit.collider.name}");
 
-        // Hide battery infront of player
+        Destroy(heldBattery);
         GetComponent<PlayerController>().movementSpeed = regularMovementSpeed;
         GetComponent<PlayerController>().sprintSpeed = regularSprintSpeed;
 
